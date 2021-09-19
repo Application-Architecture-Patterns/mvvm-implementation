adcd1f8d55e1a4b71a33742503aa2ee2a6da2d4c
pewww <jung22337@naver.com>
2021-09-19 18:06:31 +0900

    :sparkles: MVVM Implementation with Typescript

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+export { checkIsInputable } from './element';
+
+export { NODE_TYPE } from './nodeType';
+
+export {
+  MUSTACHE_REGEX,
+  MUSTACHE_INNER_VALUE_REGEX
+} from './regex';
---
+++ src/utils/regex.ts
@@ -0,0 +1,2 @@
+export const MUSTACHE_REGEX = /{{\s?([^}]*)\s?}}/g;
+export const MUSTACHE_INNER_VALUE_REGEX = /[^{\{]+(?=}\})/g;
